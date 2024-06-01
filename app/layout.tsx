22a0573935b201cef0dac5fb66297fc7dbfb0298
Stephen Hibbert <stephenhibbert92@gmail.com>
2024-06-01 07:59:14 +0100

    chore: Add SpeedInsights component for performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans as FontSans } from 'next/font/google';
 import "./globals.css";
 import Footer from "@/components/footer"; // Ensure the path and file name are correct
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const font = FontSans({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${font.className} flex min-h-screen flex-col`}>
         <main className="flex-grow">{children}</main>
+        <SpeedInsights />
         <Analytics />
         <Footer />
       </body>
